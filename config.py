1113eb429544e5dab60bc99b12d1bd5b99d572ba
huyij1013 <huyij1013@gmail.com>
2022-06-05 14:33:47 +0800

    remote sensing image classification

--- config.py
+++ config.py
@@ -2,25 +2,28 @@ import os
 
 
 
-NET_NAME = 'resnet_v1_101' #
+NET_NAME = 'resnet_v1_50' #
 
 FIXED_BLOCKS = False
 
 WEIGHT_DECAY = 0.00001
 
-num_classes = 100
+num_classes = 3
 
-global_pool = False #resnet做分类时需要设置成True
+global_pool = True #resnet做分类时需要设置成True
 
-train_path = os.path.join('data', 'train')
-test1_path = os.path.join('data', 'test1')
-test2_path = os.path.join('data', 'test2')
-cache_path = os.path.join('data', 'cache')
-cache_rebuild = False
+train_path = os.path.join(r'F:\learning\interpretation\Tensorflow-Resnet-Image-Classification-master\UCMerced_LandUse\1', 'train')
+test1_path = os.path.join(r'F:\learning\interpretation\Tensorflow-Resnet-Image-Classification-master\UCMerced_LandUse\1', 'test1')
+test2_path = os.path.join(r'F:\learning\interpretation\Tensorflow-Resnet-Image-Classification-master\UCMerced_LandUse\1', 'test2')
+cache_path = os.path.join(r'F:\learning\interpretation\Tensorflow-Resnet-Image-Classification-master\UCMerced_LandUse\1', 'cache')
+cache_rebuild = True
 batch_size = 16
-val_batch_size = 32
-image_height = 224
-image_width = 224
+
+val_batch_size = 16
+# image_height = 224
+# image_width = 224
+image_height = 256
+image_width = 256
 image_channels = 3
 
 num_iters = 70000
@@ -35,7 +38,7 @@ beta2 = 0.999
 
 momentum = 0.9
 
-save_stp = 2000
+save_stp = 1
 
 batch_norm_scale = True
 
